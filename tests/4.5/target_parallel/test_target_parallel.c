dfcd16a36fd9795ff3280ca0299d2a481efc83b7
Josh Davis <jhdavis@udel.edu>
2019-07-15 14:33:02 -0400

    added work on debugging/investigating xlc failure for this test

--- tests/4.5/target_parallel/test_target_parallel.c
+++ tests/4.5/target_parallel/test_target_parallel.c
@@ -2,8 +2,8 @@
 // 
 // OpenMP API Version 4.5 Nov 2015
 // 
-// This test checks for the combined construct target and parallel. It allows to create a
-// parallel region inside of the target device. 
+// This test checks for the combined construct target and parallel. It creates a parallel
+// region inside of the target device. 
 //
 //===----------------------------------------------------------------------------------===//
 //
@@ -17,6 +17,11 @@ int test_target_parallel() {
 
   int default_num_threads;
   int *num_threads;
+
+  int *max_threads;
+  int *thread_limit;
+  int *dynamic;
+
   int errors = 0;
 
   // We obtain the default number of threads in the target region
@@ -26,20 +31,42 @@ int test_target_parallel() {
     default_num_threads = omp_get_num_threads();
   }
 
+  //default_num_threads = 1024;
+
   OMPVV_INFOMSG("Num threads = %d", default_num_threads);
+
+  OMPVV_INFOMSG("Max threads as reported at toplevel = %d", omp_get_max_threads());
+  OMPVV_INFOMSG("Thread limit as reported at toplevel = %d", omp_get_thread_limit());
+  OMPVV_INFOMSG("Dynamic thread adjustment as reported at toplevel = %d", omp_get_dynamic());
+
   // Warning if threads is just 1. Parallel does nothing
   OMPVV_WARNING_IF(default_num_threads == 1, "The number of threads was 1. This is not a specifications error but we could not confirm the parallel region");
 
-  num_threads = (int *) malloc (sizeof(int) * default_num_threads); 
-#pragma omp target parallel map(from:num_threads[0:default_num_threads])
+  num_threads = (int *) malloc (sizeof(int) * default_num_threads);
+
+  max_threads = (int *) malloc (sizeof(int) * default_num_threads);
+  thread_limit = (int *) malloc (sizeof(int) * default_num_threads);
+  dynamic = (int *) malloc (sizeof(int) * default_num_threads);
+
+#pragma omp target parallel map(from:num_threads[0:default_num_threads], max_threads[0:default_num_threads], thread_limit[0:default_num_threads], dynamic[0:default_num_threads])
   {
     int thread_id = omp_get_thread_num();
     num_threads[thread_id] = omp_get_num_threads();
+    
+    max_threads[thread_id] = omp_get_max_threads();
+    thread_limit[thread_id] = omp_get_thread_limit();
+    dynamic[thread_id] = omp_get_dynamic();
+
   }
 
   for (int i = 0; i < default_num_threads; ++i) {
     OMPVV_TEST_AND_SET(errors, num_threads[i] != default_num_threads);
-    OMPVV_ERROR_IF(num_threads[i] != default_num_threads, "The number of threads in threadID = %d was %d. Expected was %d", i, num_threads[i], default_num_threads);
+    OMPVV_ERROR_IF(num_threads[i] != default_num_threads, "The number of threads in threadID = %d was %d. Expected was %d.", i, num_threads[i], default_num_threads);
+    
+    OMPVV_INFOMSG("The max threads reported by threadID = %d was %d.", i, max_threads[i]);
+    OMPVV_INFOMSG("The thread limit reported by threadID = %d was %d.", i, thread_limit[i]);
+    OMPVV_INFOMSG("The get_dynamic reported by threadID = %d was %d.", i, dynamic[i]);
+
   }
 
   return errors;
