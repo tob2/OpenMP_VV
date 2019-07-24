6c370734456635a7e399855be36cbef0e1612995
Josh Davis <jhdavis@udel.edu>
2019-07-24 15:02:25 -0400

    Finished basic mwe of questionable behavior

--- tests/4.5/target_parallel/max_threads_bug_mwe.c
+++ tests/4.5/target_parallel/max_threads_bug_mwe.c
@@ -16,9 +16,11 @@ int main() {
   printf("max_threads_bug_mwe.c\n");
 
   int errors = 0;
-  int first_max_threads, first_num_threads;
+  int first_max_threads, first_num_threads, first_dynamic;
+  int second_num_threads;
   int *num_threads_arr;
   int *max_threads_arr;
+  int *dynamic_arr;
 
   //Check num_ and max_threads ICVs in target region 1
 #pragma omp target parallel map(from:first_num_threads, first_max_threads)
@@ -27,39 +29,38 @@ int main() {
     {
       first_num_threads = omp_get_num_threads();
       first_max_threads = omp_get_max_threads();
+      first_dynamic = omp_get_dynamic();
     }
   }
 
-  printf("Num threads in first target region = %d\n", first_num_threads);
-  printf("Max threads in first target region = %d\n", first_max_threads);
-
   num_threads_arr = (int *) malloc(first_num_threads*sizeof(int));
   max_threads_arr = (int *) malloc(first_num_threads*sizeof(int));
+  dynamic_arr = (int *) malloc(first_num_threads*sizeof(int));
 
   //Check num_ and max_threads ICVs in target region 2
-#pragma omp target parallel map(from:num_threads_arr[0:first_num_threads], max_threads_arr[0:first_num_threads])
+#pragma omp target parallel map(from:num_threads_arr[0:first_num_threads], max_threads_arr[0:first_num_threads], dynamic_arr[0:first_num_threads])
   {
     int thread_id = omp_get_thread_num();
-    num_threads_arr[thread_id] = omp_get_num_threads(); 
-    max_threads_arr[thread_id] = omp_get_max_threads(); 
+    num_threads_arr[thread_id] = omp_get_num_threads();
+    max_threads_arr[thread_id] = omp_get_max_threads();
+    dynamic_arr[thread_id] = omp_get_dynamic();
   }
 
+  /*
   for (int i = 0; i < first_num_threads; i++) {
     if (num_threads_arr[i] != first_num_threads) {
       errors++;
       printf("Thread %d reads %d threads, should be %d\n", i, num_threads_arr[i], first_num_threads);
     }
-    if (max_threads_arr[i] != first_max_threads) {
-      errors++;
-      printf("Thread %d reads %d max threads, should be %d\n", i, max_threads_arr[i], 
-	     first_max_threads);
-    }
   }
+  */
   
   printf("Num threads r1: %d\n", first_num_threads);
   printf("Max threads r1: %d\n", first_max_threads);
+  printf("Dynamic r1: %d\n", first_dynamic);
   printf("Num threads r2: %d\n", num_threads_arr[0]);
   printf("Max threads r2: %d\n", max_threads_arr[0]);
+  printf("Dynamic r2: %d\n", dynamic_arr[0]);
 
   return errors;
 }
